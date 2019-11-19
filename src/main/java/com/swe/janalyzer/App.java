fa9067d62a71535d38b87e75deeba6da44d55acf
Leonhard Kipp <Leonhard.Kipp@web.de>
2019-11-19 21:49:50 +0100

    Call metric module from main(String[] args) directly to test

--- src/main/java/com/swe/janalyzer/App.java
+++ src/main/java/com/swe/janalyzer/App.java
@@ -1,5 +1,11 @@
 package com.swe.janalyzer;
 
+import com.swe.janalyzer.analysis.MetricCalculator;
+import com.swe.janalyzer.analysis.MetricCalculatorImpl;
+
+import java.io.IOException;
+import java.nio.file.Paths;
+
 /**
  * Hello world!
  *
@@ -8,6 +14,13 @@ public class App
 {
     public static void main( String[] args )
     {
+
+        MetricCalculator cal = new MetricCalculatorImpl();
+        try {
+            cal.calculate(Paths.get("/home/leonhard/Code/oos/p2"));
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
         if(args.length > 0){
             System.out.println("Handle Args cli");
         }else{
