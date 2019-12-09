fe64999c5d6ea7f92440ed5cd43d80622d043546
Leonhard Kipp <Leonhard.Kipp@web.de>
2019-12-09 17:57:15 +0100

    Increase cc for catch clause, don't count anonymous class decls in cc

--- src/main/java/com/swe/janalyzer/analysis/cc/CCVisitor.java
+++ src/main/java/com/swe/janalyzer/analysis/cc/CCVisitor.java
@@ -5,6 +5,7 @@ import com.github.javaparser.ast.NodeList;
 import com.github.javaparser.ast.body.ClassOrInterfaceDeclaration;
 import com.github.javaparser.ast.body.MethodDeclaration;
 import com.github.javaparser.ast.expr.MethodCallExpr;
+import com.github.javaparser.ast.expr.ObjectCreationExpr;
 import com.github.javaparser.ast.stmt.*;
 import com.github.javaparser.ast.type.ClassOrInterfaceType;
 import com.github.javaparser.ast.visitor.VoidVisitorAdapter;
@@ -15,6 +16,7 @@ import com.swe.janalyzer.util.ClassSpecifier;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Map;
+import java.util.Optional;
 
 public class CCVisitor extends VoidVisitorAdapter<Void> {
 
@@ -38,6 +40,7 @@ public class CCVisitor extends VoidVisitorAdapter<Void> {
         }
 
         final int totalCCValue = 1 + decl.stream()
+                .filter(node -> !inAnonymusFunc(node, decl))
                 .mapToInt(this::toCCValue)
                 .sum();
 
@@ -60,16 +63,52 @@ public class CCVisitor extends VoidVisitorAdapter<Void> {
         }
     }
 
+    /*
+    This Method checks upwards the tree stopping at decl latest, whether node is
+    in an anonymous class
+
+    TODO Atm every node in the AST is visited. If no anonymousExpr are visited downwards
+    no checks upwards are needed
+
+     */
+    private boolean inAnonymusFunc(Node node, MethodDeclaration decl) {
+        Optional<Node> parentNode = node.getParentNode();
+        if(!parentNode.isPresent()){
+            return false;
+        }
+        Node parent = parentNode.get();
+        //If upper bound reached
+        if(parent.equals(decl)){
+            return false;
+        }
+
+        Class parentClazz = parent.getClass();
+        if(parentClazz != ObjectCreationExpr.class){
+            //Maybe the grandparent is an anonymous class expression
+            return inAnonymusFunc(parent, decl);
+        }
+
+        ObjectCreationExpr anonymousClassExpr = (ObjectCreationExpr) parent;
+        if(anonymousClassExpr.getAnonymousClassBody().isPresent()){
+            return true;
+        }else{
+            return inAnonymusFunc(parent,decl);
+        }
+
+    }
+
     private int toCCValue(Node node){
         Class clazz = node.getClass();
         if(clazz == DoStmt.class
                 ||clazz == ExplicitConstructorInvocationStmt.class
+                || clazz == ObjectCreationExpr.class
                 || clazz == ForEachStmt.class
                 || clazz == ForStmt.class
                 || clazz == IfStmt.class
                 || clazz == WhileStmt.class
                 || clazz == TryStmt.class
                 || clazz == MethodCallExpr.class
+                || clazz == CatchClause.class
             ){
             return 1;
         } else if (clazz == SwitchStmt.class) {
