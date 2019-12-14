8b635699cd4cf565ce63919e110426a3dbe72fba
Markus Bünning <markus.buenning@alumni.fh-aachen.de>
2019-12-14 20:37:38 +0100

    methode zum Öffnen des Options-Fensters implementiert

--- src/main/java/com/swe/janalyzer/gui/StartController.java
+++ src/main/java/com/swe/janalyzer/gui/StartController.java
@@ -11,16 +11,23 @@ public class StartController {
 	@FXML private javafx.scene.control.ListView<String> history; //generischer Typ muss noch geändert werden!
 	public StartController() {}
 	
+	// Methode zum öffnen des Options-Menü
 	@FXML
 	private void openOptions() {
-		
+		Parent option = FXMLLoader.load(getClass().getResource("OptionView.fxml"));
+		Scene scene = new Scene(option, 300, 450); // initiale Fenstergröße muss noch angepasst werden
+		primaryStage.setTitle("Optionen");
+		primaryStage.setScene(scene);
+		primaryStage.show();
 	}
 	
+	// Methode zum Löschen von ausgewählten Projekten
 	@FXML
 	private void deleteSelectedProjects() {
 		
 	}
 	
+	// Methode zum öffnen des FolderPickers (Java Projekt auswählen -> analysieren -> Eintrag in History hinzufügen)
 	@FXML
 	private void openFolderPicker() {
 		
