2c64e536c118b150fdeed8b929c16ee2d5751e27
Avolus <56605781+Avolus@users.noreply.github.com>
2019-12-16 13:45:17 +0100

    Add view and model for Options

---
+++ src/main/java/com/swe/janalyzer/gui/OptionModel.java
@@ -0,0 +1,15 @@
+package com.swe.janalyzer.gui;
+
+public class OptionModel {
+
+	int LOC = 0;
+	int DIT = 0;
+	int CC = 0;
+	int WMC = 0;
+	
+	String defaultName;
+	String defaultPath;
+	String altName;
+	String altPath;
+	
+}
