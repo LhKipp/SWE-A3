18be85169d3de114f55a7608edc13f22cff185f6
Leonhard Kipp <Leonhard.Kipp@web.de>
2020-01-09 12:27:33 +0100

    Add more colors to ColorProvider and implement wrap around

--- src/main/java/com/swe/janalyzer/gui/util/ColorProvider.java
+++ src/main/java/com/swe/janalyzer/gui/util/ColorProvider.java
@@ -8,13 +8,20 @@ public class ColorProvider {
             "#89C26B",
             "#A3CCCC",
             "#DACDA7",
-            "#F2A46B"
+            "#F2A46B",
+            "#D5835E",
+            "#594153",
+            "#F5C372",
+            "#5B1A31",
+            "#55D1D2",
+            "#49C172",
+            "#5F455F"
     };
 
     private int index = -1;
 
     public String next(){
-        index = index +1  < colors.length ? index +1 : -1;
+        index = index +1  < colors.length ? index +1 : 0;
         return colors[index];
     }
 }
