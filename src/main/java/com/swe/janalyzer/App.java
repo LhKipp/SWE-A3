b345e99026cc2ae2070be2a6c326a473923a666e
Leonhard Kipp <Leonhard.Kipp@web.de>
2020-01-10 15:05:04 +0100

    Silence possible output of javafx launch

--- src/main/java/com/swe/janalyzer/App.java
+++ src/main/java/com/swe/janalyzer/App.java
@@ -5,6 +5,8 @@ import com.swe.janalyzer.gui.GuiMain;
 import javafx.application.Application;
 import javafx.stage.Stage;
 
+import java.io.ByteArrayOutputStream;
+import java.io.PrintStream;
 import java.util.List;
 
 
@@ -14,13 +16,27 @@ import java.util.List;
  */
 public class App extends Application
 {
+    private static PrintStream sout;
+    private static PrintStream serr;
     public static void main(String[] args )
     {
+//         Create a stream to hold the output
+        PrintStream pout = new PrintStream(new ByteArrayOutputStream());
+        PrintStream perr = new PrintStream(new ByteArrayOutputStream());
+        // IMPORTANT: Save the old System.out!
+        sout = System.out;
+        serr = System.err;
+        // Tell Java to use your special stream
+        System.setOut(pout);
+        System.setErr(perr);
+
         launch(args);
     }
 
     @Override
     public void start(Stage primaryStage) throws Exception {
+        System.setErr(serr);
+        System.setOut(sout);
         List<String> raw = getParameters().getRaw();
         if(raw.isEmpty()){
             new GuiMain().start(primaryStage);
