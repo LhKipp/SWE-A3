59b8b36cdcebcdc970ff0b035296787cd6d1a859
Leonhard Kipp <Leonhard.Kipp@web.de>
2020-01-17 19:13:22 +0100

    Correctly calc double to percentage

--- src/main/java/com/swe/janalyzer/gui/util/Percentage.java
+++ src/main/java/com/swe/janalyzer/gui/util/Percentage.java
@@ -1,11 +1,14 @@
 package com.swe.janalyzer.gui.util;
 
+import java.text.NumberFormat;
+
 public class Percentage {
 
 
     public static String formatAsPercentage(double value){
-        final String v = Double.toString(value * 100);
-        final int lastDigit = 5 < v.length() ? 5 : v.length();
-        return v.substring(0, lastDigit) + "%";
+        NumberFormat nf = NumberFormat.getPercentInstance();
+        nf.setMaximumFractionDigits(2);
+        nf.setMinimumFractionDigits(2);
+        return nf.format(value);
     }
 }
